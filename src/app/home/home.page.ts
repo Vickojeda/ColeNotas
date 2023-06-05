d52dd619d04ad246e5c64262128f1c704945a4c1
Vickojeda <vic.ojeda@duocuc.cl>
2023-06-05 18:12:53 -0400

    Cambios proyecto

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage 
 
-  constructor() {}
+  constructor(private router: Router) {
+    const navigation = this.router.getCurrentNavigation();
+    const state = navigation?.extras.state as {
+     
+  }
 
 }
