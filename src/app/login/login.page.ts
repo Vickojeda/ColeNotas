2ba93e477883e7a19c700749783af180339b095f
Vickojeda <vic.ojeda@duocuc.cl>
2023-06-05 19:36:34 -0400

    commit de animacion

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,6 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
-import { NavController } from '@ionic/angular';
+import { Component, OnInit, ElementRef, ViewChild } from '@angular/core';
+import { AnimationController } from "@ionic/angular"
 
 @Component({
   selector: 'app-login',
@@ -9,12 +8,36 @@ import { NavController } from '@ionic/angular';
 })
 export class LoginPage implements OnInit {
 
-  constructor(private router: Router) {
-    
-   }
+  @ViewChild("sign", { read: ElementRef, static: true }) sign: ElementRef
+  @ViewChild('help', { read: ElementRef }) help: ElementRef;
 
-  goHome() {
-    this.router.navigate(['/home'], { state: {lalo: "test"} })
+  constructor(private animationCtrl: AnimationController) {}
+
+   ngAfterViewInit() {
+    this.pulseButton()
+  }
+
+  public pulseButton() {
+    const animation = this.animationCtrl
+      .create()
+      .addElement(this.sign.nativeElement)
+      .duration(1500)
+      .iterations(Infinity)
+      .keyframes([
+        { offset: 0, boxShadow: "0 0 0 0 rgba(44, 103, 255, 0.4)" },
+        { offset: 0.7, boxShadow: "0 0 0 10px rgba(44, 103, 255, 0)" },
+        { offset: 1, boxShadow: "0 0 0 0 rgba(44, 103, 255, 0)" }
+      ]);
+
+
+    const loadingAnimation = this.animationCtrl.create()
+      .addElement(this.help.nativeElement)
+      .duration(1500)
+      .iterations(Infinity)
+      .fromTo('opacity', '1', '0.1');
+
+    loadingAnimation.play();
+    animation.play();
   }
 
   ngOnInit() {
