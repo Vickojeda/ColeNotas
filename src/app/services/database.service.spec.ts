87f5797ff657847af49a28878f63b5a770d52b68
Vickojeda <vic.ojeda@duocuc.cl>
2023-07-10 23:16:54 -0400

    ejecutables

--- src/app/services/database.service.spec.ts
+++ src/app/services/database.service.spec.ts
@@ -1,12 +1,17 @@
 import { TestBed } from '@angular/core/testing';
 
 import { DatabaseService } from './database.service';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 describe('DatabaseService', () => {
   let service: DatabaseService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [
+        HttpClientTestingModule 
+      ]
+    }).compileComponents();
     service = TestBed.inject(DatabaseService);
   });
 
